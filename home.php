7326a11b99b8a61aef29a07ec1109697ea117d49
Abishek Bhat <bhat.abishek@gmail.com>
2012-01-06 15:14:08 +0530

    new file:   home.php Test file to see if Limonade was working. 	new file:   index.php added places support 	modified:   lib/foursquare (modified content) Bug fixes

---
+++ home.php
@@ -0,0 +1,16 @@
+<?php
+
+ require_once('lib/limonade.php');
+
+ $shit="This so scary";
+ home($shit);
+
+ function home($shit)
+{
+   echo $shit;
+}
+
+//phpinfo();
+
+?>
+
---
+++ index.php
@@ -0,0 +1,66 @@
+<?php
+
+ require_once('lib/limonade.php');
+ require_once('lib/facebook/src/facebook.php');
+ require_once('lib/foursquare/src/FoursquareAPI.class.php');
+ 
+
+ dispatch('/','home');
+
+ function home()
+{
+ $facebook=new Facebook(array("appid"=>"","appsecret"=>""));
+  
+
+  $user=$facebook->getUser();
+}
+ dispatch('/findPalce/:partner/:place','getPlace');
+ function getPlace()
+{
+  $friends=$facebook->api('/me/friends/','GET');
+  $friends_a=json_decode($friends,true);
+  foreach($friends_a as $friend)
+{
+   if(params('partner')==$friend['name'])
+    {
+      $partner=$friend;
+      //getCheckin($partner);
+     }
+   else 
+    {
+      echo "Boy that takes some guts to ask out a girl,who is not even in your facebook list.";
+     }
+}
+$location=params('place');
+getSuggestions($location);
+}
+
+ function getCheckin($partner)
+{
+   $checkins=$facebook->api('/'+$partner['id']+'/checkins/'.'GET');
+   $checkins_a=json_decode($checkins,true);
+ }
+
+ function getSuggestions($location)
+{
+   $client_key="3BCHYET02RP3D4TYEVQLVDUPAKHMAF3CRT45X3MAVJREB11Q";
+   $client_secret="T4DREJ31ZABXTOXOQWESYFEDSKQ3COOENUAPB0V2TS3OLWMA";
+   $four=new FoursquareAPI($client_key,$client_secret);
+   list($lat,$long)=$four->GeoLocate($location);
+   $coords=array("ll"=>"$lat,$lang");
+   $response=$four->GetPublic("/venues/search",$coords);
+   $venues=json_decode($reponse);
+   foreach($venues->response->venues as $venue)
+{
+   if($venues->categories->name==("Restuarants"||"Dinner Places"||"Shopping Mall"||"Bowling Alley"||"Theatres"||"Multiplex"||"Coffee-House"));
+   {
+     $place->name=$venue->name;
+     $place->id=$venue->id;
+     $place->location=$venue->location->address;
+    
+   }
+  
+}
+}
+?>
+
